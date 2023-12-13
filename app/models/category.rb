c058eb85845e7fa1fb415cdcc3881e8901fb82a5
Alice Laurin <laurin.alice@gmail.com>
2023-12-13 13:53:39 +0100

    model et migrations bis

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   CATEGORIES = ["bombe", "mignonne", "pas ouf", "deg"]
   belongs_to :miss
   belongs_to :user
-  validates :status, inclusion: { in:  CATEGORIES}
+  validates :type, inclusion: { in:  CATEGORIES}
 end
