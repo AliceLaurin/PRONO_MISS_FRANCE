4cd71eaa2bf8637272a777ed7c970497bfa42ef1
Alice Laurin <laurin.alice@gmail.com>
2023-12-14 18:23:15 +0100

    groupuser create when create group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,14 +2,14 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @year = Year.find(params[:year_id])
-
   end
 
   def create
     @year = Year.find(params[:year_id])
     @group = Group.new(params_group)
-
+    @group.year = @year
     if @group.save
+      GroupUser.create!(group_id: @group.id, user_id: current_user.idg)
       redirect_to year_group_path(@year, @group)
     else
       render :new, status: :unprocessable_entity
