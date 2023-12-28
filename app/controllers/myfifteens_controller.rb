3ad00051dd7d3bcc660758fb1c32bb4af29cbf99
Alice Laurin <laurin.alice@gmail.com>
2023-12-28 18:53:58 +0100

    turbo stream

--- app/controllers/myfifteens_controller.rb
+++ app/controllers/myfifteens_controller.rb
@@ -1,20 +1,18 @@
 class MyfifteensController < ApplicationController
+
     def index
       @year = Year.find(params[:year_id])
-      @all_my_fifteens = Myfifteen.where(user_id: current_user, year_id: @year.id)
+      @all_my_fifteens = Myfifteen.where(user_id: current_user, year_id: @year.id).to_a
       all_my_groups = current_user.groups.to_a
-      @this_year_groups = all_my_groups.select do |group|
-        group.year_id == @year.id
-      end
+      @this_year_groups = all_my_groups.select { |group| group.year_id == @year.id }
       @my_top_15 = Myfifteen.where(user_id: current_user.id).pluck(:miss_id).uniq
-    end
 
-    def new_modal
-      @year = Year.find(params[:year_id])
+      # Add this line to output debug information
+      puts "@all_my_fifteens: #{@all_my_fifteens.inspect}"
     end
 
     def new
-      @new_fifteen = Myfifteen.new
+      @myfifteen = Myfifteen.new
       @year = Year.find(params[:year_id])
       @misses = Miss.where(year_id: @year.id)
                     .joins("LEFT JOIN myfifteens ON misses.id = myfifteens.miss_id AND myfifteens.user_id = #{current_user.id}")
@@ -30,17 +28,26 @@ class MyfifteensController < ApplicationController
 
 
     def create
-      @new_fifteen = Myfifteen.new(myfifteen_params)
       @year = Year.find(params[:year_id])
+      @myfifteen = Myfifteen.new(myfifteen_params.merge(user_id: current_user.id, year_id: @year.id))
 
-      @new_fifteen.miss = Miss.find(params[:myfifteen][:miss_id])
-      @new_fifteen.user_id = current_user.id
-      @new_fifteen.year_id = @year.id
+
+      @myfifteen.miss = Miss.find(params[:myfifteen][:miss_id])
+      @myfifteen.user_id = current_user.id
+      @myfifteen.year_id = @year.id
       existing_guess = Myfifteen.find_by(user_id: current_user.id, my_miss_france_guess: "1")
       existing_guess.update(my_miss_france_guess: "0") if existing_guess.present?
 
-      if  @new_fifteen.save!
-        redirect_to year_myfifteens_path
+      if  @myfifteen.save!
+        # Reassign @all_my_fifteens after creating a new Myfifteen record
+        @all_my_fifteens = Myfifteen.where(user_id: current_user, year_id: @year.id).to_a
+        # Add debug output
+        puts "@all_my_fifteens after reassignment: #{@all_my_fifteens.inspect}"
+
+        respond_to do |format|
+          format.html { redirect_to year_myfifteens_path(@year) }
+          format.turbo_stream
+        end
       else
         render :new, status: :unprocessable_entity
       end
